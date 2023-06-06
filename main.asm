e96160cc575042119059fe9da73b4b5d922efbba
root <root@ShucePC.localdomain>
2023-06-06 23:06:42 +0800

    Format indents

--- main.asm
+++ main.asm
@@ -18,86 +18,86 @@ init_board:
 	mov r1, #0x9
 	mov r2, #0x0
 	reset_board_loop:
-        strb r2, [r0], #0x1
-        subs r1, #0x1
-        bne reset_board_loop
-    bx lr
+		strb r2, [r0], #0x1
+		subs r1, #0x1
+		bne reset_board_loop
+	bx lr
 
  game_loop:
- 	bl display_board
- 	bl get_input
- 	bl check_win
- 	bl check_all_filled
- 	bl switch_player
- 	bl game_loop
+	bl display_board
+	bl get_input
+	bl check_win
+	bl check_all_filled
+	bl switch_player
+	bl game_loop
 
  display_board:
- 	mov r7, #0x4
-    mov r0, #0x1
-    mov r2, #0x1
-    ldr r1, =newline
-    swi 0
+	mov r7, #0x4
+	mov r0, #0x1
+	mov r2, #0x1
+	ldr r1, =newline
+	swi 0
 
-    ldr r4, =board
-    mov r5, #0x3
+	ldr r4, =board
+	mov r5, #0x3
 
-    display_loop:
-        mov r6, #0x3
-        display_inner_loop:
-        	ldrb r8, [r4], #0x1
-        	cmp r8, #0x0
-        	ldreq r1, =empty
-        	cmp r8, #0x1
-        	ldreq r1, =X
-        	cmp r8, #0x2
-        	ldreq r1, =O
-            swi 0
-            subs r6, #0x1
-            bne display_inner_loop
-        ldr r1, =newline
-        swi 0
-        subs r5, #0x1
-        bne display_loop
+	display_loop:
+		mov r6, #0x3
+		display_inner_loop:
+			ldrb r8, [r4], #0x1
+			cmp r8, #0x0
+			ldreq r1, =empty
+			cmp r8, #0x1
+			ldreq r1, =X
+			cmp r8, #0x2
+			ldreq r1, =O
+			swi 0
+			subs r6, #0x1
+			bne display_inner_loop
+		ldr r1, =newline
+		swi 0
+		subs r5, #0x1
+		bne display_loop
 
-    ldr r1, =newline
-    swi 0
+	ldr r1, =newline
+	swi 0
 
-    bx lr
+	bx lr
 
 get_input:
- 	mov r7, #0x4
- 	mov r0, #0x1
- 	mov r2, #0x8
- 	cmp r9, #0x1
- 	ldreq r1, =Xturn
- 	ldrne r1, =Oturn
- 	swi 0
+	mov r7, #0x4
+	mov r0, #0x1
+	mov r2, #0x8
+	cmp r9, #0x1
+	ldreq r1, =Xturn
+	ldrne r1, =Oturn
+	swi 0
 
- 	mov r7, #0x3
- 	mov r0, #0x0
- 	ldr r1, =input
- 	mov r2, #0x2
- 	swi 0
+	mov r7, #0x3
+	mov r0, #0x0
+	ldr r1, =input
+	mov r2, #0x2
+	swi 0
 
- 	ldrb r1, [r1]
- 	cmp r1, #'0'
- 	ble get_input
- 	cmp r1, #'9'
- 	bgt get_input
- 	sub r1, #'1'
+	ldrb r1, [r1]
+	cmp r1, #'0'
+	ble get_input
+	cmp r1, #'9'
+	bgt get_input
+	sub r1, #'1'
 
- 	ldr r0, =board
- 	ldrb r2, [r0, r1]!
- 	cmp r2, #0x0
- 	bne get_input
- 	strb r9, [r0]
- 	bx lr
+	ldr r0, =board
+	ldrb r2, [r0, r1]!
+	cmp r2, #0x0
+	bne get_input
+	strb r9, [r0]
+	bx lr
 
 switch_player:
- 	cmp r9, #0x1
- 	moveq r9, #0x2
- 	movne r9, #0x1
- 	bx lr
+	cmp r9, #0x1
+	moveq r9, #0x2
+	movne r9, #0x1
+	bx lr
 
 check_all_filled:
 	ldr r0, =board
